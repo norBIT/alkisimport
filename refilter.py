a794f9e1bffc2d96a1fd22ed3ddb4f07f4909499
Juergen E. Fischer <jef@norbit.de>
2014-04-17 11:08:40 +0200

    refilter.py: ohne parameter stdin filtern

--- refilter.py
+++ refilter.py
@@ -2,19 +2,22 @@
 # -*- coding: utf8 -*-
 
 import re
-import sys 
-import os 
+import sys
+import os
 
 f = open(os.path.join( os.path.dirname(__file__), "re"), "rbU")
 pattern = re.compile( f.read().strip("\n").replace("\n","|") )
 f.close()
 
-if len(sys.argv)!=2:
-	print "usage: %s logfile" % (sys.argv[0])
+if len(sys.argv)>2:
+	print "usage: %s [logfile]" % (sys.argv[0])
 	exit(1)
 
 try:
-	f = open(sys.argv[1], "rbU")
+	if len(sys.argv)==2:
+		f = open(sys.argv[1], "rbU")
+	else:
+		f = os.fdopen(0, "rbU")
 except:
 	print "%s: could not open %s" % (sys.argv[0], sys.argv[1])
 	exit(1)
