8ef4d1f5537454db598d8fff4faf103fe3525a96
Juergen E. Fischer <jef@norbit.de>
2020-02-08 10:34:16 +0100

    ALB: bvnr mit generierter laufenden Nummer füllen (closes #27)

--- postprocessing.d/4_nas2alb/3_eignerart.sql
+++ postprocessing.d/4_nas2alb/3_eignerart.sql
@@ -7,36 +7,39 @@ SET search_path = :"alkis_schema", :"parent_schema", :"postgis_schema", public;
 DELETE FROM eignerart;
 INSERT INTO eignerart(flsnr,bestdnr,bvnr,b,anteil,auftlnr,sa,ff_entst,ff_stand,lkfs)
 	SELECT
-		alkis_flsnr(f) AS flsnr,
-		to_char(alkis_toint(bb.land),'fm00') || to_char(alkis_toint(bb.bezirk),'fm0000') || '-' || trim(bb.buchungsblattnummermitbuchstabenerweiterung) AS bestdnr,
-		lpad(substr(laufendenummer,length(laufendenummer)-3),4,'0') AS bvnr,
-		buchungsart AS b,
-		coalesce(bs.zaehler || '/' || bs.nenner,bs.zaehler::text) AS anteil,
-		laufendenummer AS auftrlnr,
-		NULL AS sa,
-		0 AS ff_entst,
-		0 AS ff_stand,
-		NULL AS lkfs
-	FROM ax_flurstueck f
-	JOIN ax_buchungsstelle bs ON bs.gml_id=f.istgebucht AND bs.endet IS NULL
-	JOIN ax_buchungsblatt bb ON bb.gml_id=bs.istbestandteilvon AND bb.endet IS NULL
-	WHERE f.endet IS NULL
+		flsnr,bestdnr,
+		to_char(row_number() OVER (PARTITION BY bestdnr ORDER BY auftrlnr), 'fm0000') AS bvnr,
+		b,anteil,auftrlnr,sa,ff_entst,ff_stand,lkfs
+	FROM (
+		SELECT
+			alkis_flsnr(f) AS flsnr,
+			to_char(alkis_toint(bb.land),'fm00') || to_char(alkis_toint(bb.bezirk),'fm0000') || '-' || trim(bb.buchungsblattnummermitbuchstabenerweiterung) AS bestdnr,
+			buchungsart AS b,
+			coalesce(bs.zaehler || '/' || bs.nenner,bs.zaehler::text) AS anteil,
+			laufendenummer AS auftrlnr,
+			NULL AS sa,
+			0 AS ff_entst,
+			0 AS ff_stand,
+			NULL AS lkfs
+		FROM ax_flurstueck f
+		JOIN ax_buchungsstelle bs ON bs.gml_id=f.istgebucht AND bs.endet IS NULL
+		JOIN ax_buchungsblatt bb ON bb.gml_id=bs.istbestandteilvon AND bb.endet IS NULL
+		WHERE f.endet IS NULL
 	UNION
-	SELECT
-		alkis_flsnr(f) AS flsnr,
-		to_char(alkis_toint(bb.land),'fm00') || to_char(alkis_toint(bb.bezirk),'fm0000') || '-' || trim(bb.buchungsblattnummermitbuchstabenerweiterung) AS bestdnr,
-		lpad(substr(bs.laufendenummer,length(bs.laufendenummer)-3),4,'0') AS bvnr,
-		bs.buchungsart AS b,
-		coalesce(bs.zaehler || '/' || bs.nenner, bs.zaehler::text) AS anteil,
-		-- bs.nummerimaufteilungsplan AS auftrlnr,
-		bs.laufendenummer AS auftrlnr,
-		NULL AS sa,
-		0 AS ff_entst,
-		0 AS ff_stand,
-		NULL AS lkfs
-	FROM ax_flurstueck f
-	JOIN ax_buchungsstelle bs0 ON bs0.gml_id=f.istgebucht AND bs0.endet IS NULL
-	JOIN ax_buchungsstelle bs  ON ARRAY[bs0.gml_id] <@ bs.an AND bs.endet IS NULL
-	JOIN ax_buchungsblatt bb ON bb.gml_id=bs.istbestandteilvon AND bb.endet IS NULL
-	WHERE f.endet IS NULL
+		SELECT
+			alkis_flsnr(f) AS flsnr,
+			to_char(alkis_toint(bb.land),'fm00') || to_char(alkis_toint(bb.bezirk),'fm0000') || '-' || trim(bb.buchungsblattnummermitbuchstabenerweiterung) AS bestdnr,
+			bs.buchungsart AS b,
+			coalesce(bs.zaehler || '/' || bs.nenner, bs.zaehler::text) AS anteil,
+			bs.laufendenummer AS auftrlnr,
+			NULL AS sa,
+			0 AS ff_entst,
+			0 AS ff_stand,
+			NULL AS lkfs
+		FROM ax_flurstueck f
+		JOIN ax_buchungsstelle bs0 ON bs0.gml_id=f.istgebucht AND bs0.endet IS NULL
+		JOIN ax_buchungsstelle bs  ON ARRAY[bs0.gml_id] <@ bs.an AND bs.endet IS NULL
+		JOIN ax_buchungsblatt bb ON bb.gml_id=bs.istbestandteilvon AND bb.endet IS NULL
+		WHERE f.endet IS NULL
+	) AS foo
 	;
