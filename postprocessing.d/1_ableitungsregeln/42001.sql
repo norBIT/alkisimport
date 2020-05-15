1f5a89069d4d1a219c0aafbc8a95d98e33ca5de5
Juergen E. Fischer <jef@norbit.de>
2020-05-15 12:07:46 +0200

    Ableitungsregeln für ax_strassenverkehr wiederhergestellt (followup 626610537)

---
+++ postprocessing.d/1_ableitungsregeln/42001.sql
@@ -0,0 +1,77 @@
+SET client_encoding TO 'UTF8';
+SET search_path = :"alkis_schema", :"parent_schema", :"postgis_schema", public;
+
+--
+-- Straßenverkehr (42001)
+--
+
+-- Straßenverkehr, Fläche
+INSERT INTO po_polygons(gml_id,thema,layer,polygon,signaturnummer,modell)
+SELECT
+	gml_id,
+	'Verkehr' AS thema,
+	'ax_strassenverkehr' AS layer,
+	st_multi(wkb_geometry) AS polygon,
+	CASE
+	WHEN coalesce(zustand,0)<>4000 THEN 25150000
+	ELSE 25160000
+	END
+	+
+	CASE
+	WHEN funktion IN (2312,2313) AND coalesce(zustand,0)<>4000 THEN 1406
+	WHEN funktion=5130           AND coalesce(zustand,0)<>4000 THEN 1414
+	ELSE 0
+	END AS signaturnummer,
+	advstandardmodell||sonstigesmodell
+FROM ax_strassenverkehr;
+
+-- Straßenverkehr, Funktion
+INSERT INTO po_labels(gml_id,thema,layer,point,text,signaturnummer,drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,modell)
+SELECT
+	gml_id,
+	'Verkehr' AS thema,
+	'ax_strassenverkehr' AS layer,
+	point,
+	text,
+	signaturnummer,
+	drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,modell
+FROM (
+	SELECT
+		o.gml_id,
+		coalesce(t.wkb_geometry,st_centroid(o.wkb_geometry)) AS point,
+		coalesce(
+			t.schriftinhalt,
+			(SELECT beschreibung FROM ax_funktion_strasse WHERE wert=funktion)
+		) AS text,
+		coalesce(d.signaturnummer,t.signaturnummer,'4100') AS signaturnummer,
+		drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,
+		coalesce(t.advstandardmodell||t.sonstigesmodell,o.advstandardmodell||o.sonstigesmodell) AS modell
+	FROM ax_strassenverkehr o
+	LEFT OUTER JOIN ap_pto t ON ARRAY[o.gml_id] <@ t.dientzurdarstellungvon AND t.art='FKT' AND t.endet IS NULL
+	LEFT OUTER JOIN ap_darstellung d ON ARRAY[o.gml_id] <@ d.dientzurdarstellungvon AND d.art='FKT' AND d.endet IS NULL
+	WHERE  funktion=4130 AND o.endet IS NULL
+) AS n WHERE NOT text IS NULL;
+
+-- Straßenverkehr, Zweitname
+INSERT INTO po_labels(gml_id,thema,layer,point,text,signaturnummer,drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,modell)
+SELECT
+	gml_id,
+	'Verkehr' AS thema,
+	'ax_strassenverkehr' AS layer,
+	point,
+	text,
+	signaturnummer,
+	drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,modell
+FROM (
+	SELECT
+		o.gml_id,
+		coalesce(t.wkb_geometry,st_centroid(o.wkb_geometry)) AS point,
+		coalesce(t.schriftinhalt,o.zweitname) AS text,
+		coalesce(d.signaturnummer,t.signaturnummer,'4141') AS signaturnummer,
+		drehwinkel,horizontaleausrichtung,vertikaleausrichtung,skalierung,fontsperrung,
+		coalesce(t.advstandardmodell||t.sonstigesmodell,o.advstandardmodell||o.sonstigesmodell) AS modell
+	FROM ax_strassenverkehr o
+	LEFT OUTER JOIN ap_pto t ON ARRAY[o.gml_id] <@ t.dientzurdarstellungvon AND t.art='ZNM' AND t.endet IS NULL
+	LEFT OUTER JOIN ap_darstellung d ON ARRAY[o.gml_id] <@ d.dientzurdarstellungvon AND d.art='ZNM' AND d.endet IS NULL
+	WHERE o.endet IS NULL
+) AS n WHERE NOT text IS NULL;
