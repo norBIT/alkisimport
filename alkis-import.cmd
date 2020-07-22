bfdb626876eb4d1e1e0469d6ab8c04ec6d7bdfab
Juergen E. Fischer <jef@norbit.de>
2020-07-22 11:59:21 +0200

    osgeo4w: alkis-import-gid7 mit gdal-dev (nightly) ausführen

--- alkis-import.cmd
+++ alkis-import.cmd
@@ -2,5 +2,6 @@
 call "%~dp0\o4w_env.bat"
 call "%~dp0\qt5_env.bat"
 call "%~dp0\py3_env.bat"
-cd "%OSGEO4W_ROOT%\apps\alkis-import"
+call "%~dp0\gdal-dev-env.bat"
+cd "%OSGEO4W_ROOT%\apps\alkis-import-gid7"
 start "ALKIS-Import" /B pythonw alkisImport.py
