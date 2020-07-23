3d3a834869e65f43a205df9f3555f47149c8107a
Juergen E. Fischer <jef@norbit.de>
2020-07-23 18:50:26 +0200

    GID7-Verknüpfung umbenannt

--- postinstall.bat
+++ postinstall.bat
@@ -2,5 +2,5 @@ REM get short path without blanks
 for %%i in ("%OSGEO4W_ROOT%") do set O4W_ROOT=%%~fsi
 
 if not %OSGEO4W_MENU_LINKS%==0 mkdir "%OSGEO4W_STARTMENU%"
-if not %OSGEO4W_MENU_LINKS%==0 nircmd shortcut "%O4W_ROOT%\bin\nircmd.exe" "%OSGEO4W_STARTMENU%" "norGIS ALKIS Import" "exec hide %O4W_ROOT%\bin\alkis-import.cmd" "%O4W_ROOT%\apps\alkis-import\logo.ico"
-if not %OSGEO4W_DESKTOP_LINKS%==0 nircmd shortcut "%O4W_ROOT%\bin\nircmd.exe" "~$folder.desktop$" "norGIS ALKIS Import" "exec hide %O4W_ROOT%\bin\alkis-import.cmd" "%O4W_ROOT%\apps\alkis-import\logo.ico"
+if not %OSGEO4W_MENU_LINKS%==0 nircmd shortcut "%O4W_ROOT%\bin\nircmd.exe" "%OSGEO4W_STARTMENU%" "norGIS ALKIS Import (GID7)" "exec hide %O4W_ROOT%\bin\alkis-import-gid7.cmd" "%O4W_ROOT%\apps\alkis-import-gid7\logo.ico"
+if not %OSGEO4W_DESKTOP_LINKS%==0 nircmd shortcut "%O4W_ROOT%\bin\nircmd.exe" "~$folder.desktop$" "norGIS ALKIS Import (GID7)" "exec hide %O4W_ROOT%\bin\alkis-import-gid7.cmd" "%O4W_ROOT%\apps\alkis-import-gid7\logo.ico"
--- preremove.bat
+++ preremove.bat
@@ -1,2 +1,2 @@
-del "%OSGEO4W_STARTMENU%\norGIS ALKIS Import.lnk"
-del "%ALLUSERSPROFILE%\Desktop\norGIS ALKIS Import.lnk"
+del "%OSGEO4W_STARTMENU%\norGIS ALKIS Import (GID7).lnk"
+del "%ALLUSERSPROFILE%\Desktop\norGIS ALKIS Import (GID7).lnk"
