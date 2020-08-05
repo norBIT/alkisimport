6239f3eae7fa4af37fc964872a54656f16d0d900
Juergen E. Fischer <jef@norbit.de>
2020-08-06 01:55:07 +0200

    [BE] Unterstützung für besondere Höhenpunkte in Berlin

---
+++ postprocessing.d/1_ableitungsregeln/62090.sql
@@ -0,0 +1,20 @@
+SET client_encoding TO 'UTF8';
+SET search_path = :"alkis_schema", :"parent_schema", :"postgis_schema", public;
+
+--
+-- Besonderer Höhenpunkt (62090)
+--
+
+SELECT 'Besondere Höhenpunkte werden verarbeitet (BE).';
+
+INSERT INTO po_points(gml_id,thema,layer,point,drehwinkel,signaturnummer,modell)
+SELECT
+	gml_id,
+	'Topographie' AS thema,
+	'ax_besondererhoehenpunkt' AS layer,
+	wkb_geometry AS point,
+	0 AS drehwinkel,
+	'BE3000' AS signaturnummer,
+	advstandardmodell||sonstigesmodell
+FROM ax_besondererhoehenpunkt
+WHERE endet IS NULL;
