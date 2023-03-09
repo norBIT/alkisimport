90273190a390b4901add23f4944dae352085fa39
Juergen E. Fischer <jef@norbit.de>
2023-03-10 00:43:36 +0100

    alkisImport.py: Korrektur bei Protokollierung postinstall.bat: config.sql im Hauptverzeichnis anlegen (für Konfiguration von Benutzer-Skripten)

--- postinstall.bat
+++ postinstall.bat
@@ -5,3 +5,5 @@ if not %OSGEO4W_DESKTOP_LINKS%==0 if not exist "%OSGEO4W_DESKTOP%" mkdir "%OSGEO
 
 if not %OSGEO4W_MENU_LINKS%==0 xxmklink "%OSGEO4W_STARTMENU%\norGIS ALKIS Import@SHORTCUT@.lnk" "%OSGEO4W_ROOT%\bin\bgspawn.exe" "%OSGEO4W_ROOT%\bin\@PKG@.cmd" . "" 1 "%OSGEO4W_ROOT%\\apps\\@PKG@\\logo.ico"
 if not %OSGEO4W_DESKTOP_LINKS%==0 xxmklink "%OSGEO4W_DESKTOP%\norGIS ALKIS Import@SHORTCUT@.lnk" "%OSGEO4W_ROOT%\bin\bgspawn.exe" "%OSGEO4W_ROOT%\bin\@PKG@.cmd" . "" 1 "%OSGEO4W_ROOT%\\apps\\@PKG@\\logo.ico"
+
+if not exist %OSGEO4W_ROOT%\apps\@PKG@\config.sql echo -- Konfiguration hier>%OSGEO4W_ROOT%\apps\@PKG@\config.sql
