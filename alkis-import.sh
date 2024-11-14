d14ce43192da2d28006a591d779709bcf0b24f06
Juergen E. Fischer <jef@norbit.de>
2024-11-14 14:04:36 +0100

    Transformation auf ein gemeinsames Koordinatensystem unterstützen (auch Non-UTM)

--- alkis-import.sh
+++ alkis-import.sh
@@ -522,6 +522,7 @@ EOF
 				-v alkis_avoiddupes=$AVOIDDUPES \
 				-v alkis_hist=$HISTORIE \
 				-v alkis_epsg=$EPSG \
+				-v alkis_transform=$TRANSFORM \
 				-v alkis_schema=$SCHEMA \
 				-v postgis_schema=$PGSCHEMA \
 				-v parent_schema=${PARENTSCHEMA:-$SCHEMA} \
@@ -545,6 +546,7 @@ EOF
 				-v alkis_avoiddupes=$AVOIDDUPES \
 				-v alkis_hist=$HISTORIE \
 				-v alkis_epsg=$EPSG \
+				-v alkis_transform=$TRANSFORM \
 				-v alkis_schema=$SCHEMA \
 				-v postgis_schema=$PGSCHEMA \
 				-v parent_schema=${PARENTSCHEMA:-$SCHEMA} \
@@ -734,6 +736,25 @@ EOF
 		continue
 		;;
 
+	"transform "*)
+		TRANSFORM=${src#transform }
+		case "${TRANSFORM,,}" in
+		an|on|true|an)
+			TRANSFORM=true
+			;;
+		aus|off|false)
+			TRANSFORM=false
+			;;
+		*)
+			echo "$P: Ungültiger Wert $TRANSFORM (true oder false erwartet)"
+			exit 1
+			;;
+		esac
+
+		continue
+		;;
+
+
 	"epsg "*)
 		EPSG=${src#epsg }
 
@@ -749,6 +770,7 @@ EOF
 				export CRS="-s_srs $B/1$EPSG.prj -t_srs EPSG:$EPSG"
 				;;
 			*)
+				export CRS="-a_srs EPSG:$EPSG"
 				;;
 			esac
 
@@ -764,6 +786,8 @@ EOF
 				export PROJ_LIB=$B CRS="-s_srs +init=custom:1$EPSG -t_srs EPSG:$EPSG"
 				;;
 			*)
+				export CRS="-a_srs EPSG:$EPSG"
+
 				;;
 			esac
 
---
+++ postcreate.d/1_transform.sql
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *                                                                         *
+ * Project:  norGIS ALKIS Import                                           *
+ * Purpose:  Transformation der Daten in ein einheitliches KBS             *
+ * Author:   Jürgen E. Fischer <jef@norbit.de>                             *
+ *                                                                         *
+ ***************************************************************************
+ * Copyright (c) 2024, Jürgen E. Fischer <jef@norbit.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+\if :alkis_transform
+
+SET search_path TO :"alkis_schema",:"postgis_schema",public;
+
+SELECT 'Eingabedaten werde in ' || :'alkis_epsg' || ' transformiert.';
+
+SELECT format('
+CREATE OR REPLACE FUNCTION inplace_transform() RETURNS TRIGGER LANGUAGE plpgsql AS $$
+BEGIN
+  IF substr(NEW.gml_id, 3, 2) IN (''BW'',''BY'',''HB'',''HE'',''HH'',''NW'',''RP'',''SH'',''SL'',''ST'',''TH'') THEN
+    NEW.wkb_geometry := st_transform(st_setsrid(NEW.wkb_geometry, 25832), %s);
+  ELSE
+    NEW.wkb_geometry := st_transform(st_setsrid(NEW.wkb_geometry, 25833), %s);
+  END IF;
+  RETURN NEW;
+END;
+$$ SET search_path TO %I,%I;
+', :alkis_epsg, :alkis_epsg, :'alkis_schema', :'postgis_schema');
+\gexec
+
+SELECT format(E'DROP TRIGGER IF EXISTS %I ON %I.%I;\nCREATE TRIGGER %I BEFORE INSERT ON %I.%I FOR EACH ROW EXECUTE PROCEDURE inplace_transform();',
+        a.table_name || '_transform', a.table_schema, a.table_name,
+        a.table_name || '_transform', a.table_schema, a.table_name)
+    FROM information_schema.columns a
+    JOIN information_schema.columns b ON a.table_schema=b.table_schema AND a.table_name=b.table_name AND b.column_name='wkb_geometry'
+    JOIN information_schema.tables c ON b.table_schema=c.table_schema AND b.table_name=c.table_name AND c.table_type='BASE TABLE'
+    WHERE a.table_schema=:'alkis_schema'
+      AND substr(a.table_name,1,3) IN ('ax_','ap_','ln_','lb_','au_','aa_')
+      AND a.column_name='gml_id';
+\gexec
+
+\endif
