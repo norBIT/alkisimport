4071bfd25036f626b4782becfb04dabb2273de81
Juergen E. Fischer <jef@norbit.de>
2025-04-03 11:21:52 +0200

    * OGR_PG_SKIP_CONFLICTS erst ab PostgreSQL 9.5 verwenden * Importierte Dateien in der Datenbank protokollieren * Erzeugung der Quittierung korrigiert * Nur eine Warnung bei Parallelimports ohne OGR_PG_SKIP_CONFLICTS * alkis_dropobject: auch Trigger unterstützen * Fix für ax_lagebezeichnungkatalogeintrag mit nicht nummerischen   Schlüsseln

---
+++ postupdate.d/1_importe.sql
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *                                                                         *
+ * Project:  norGIS ALKIS Import                                           *
+ * Purpose:  Zeitstempel der importierten Daten aufzeichnen                *
+ * Author:   Jürgen E. Fischer <jef@norbit.de>                             *
+ *                                                                         *
+ ***************************************************************************
+ * Copyright (c) 2025, Jürgen E. Fischer <jef@norbit.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+SET search_path TO :"alkis_schema",:"postgis_schema",public;
+
+SELECT count(*)>1 AS exists FROM information_schema.tables WHERE table_schema=:'alkis_schema' AND table_name='alkis_importe';
+\gset
+
+\if :exists
+\else
+\ir ../postcreate.d/1_importe.sql
+\endif
